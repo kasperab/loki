8554ec2a57c686f4d279342f0e4a3490122a707f
kasperab <kasperab@yahoo.se>
2026-01-31 15:54:49 +0100

    Added background music dependent on player form

--- scripts/mask_menu.gd
+++ scripts/mask_menu.gd
@@ -2,26 +2,31 @@ extends VBoxContainer
 
 @onready var player = $".."
 @onready var sprite = $"../AnimatedSprite2D"
+@onready var loki_music = $"../LokiMusic"
+@onready var horse_music = $"../HorseMusic"
 
 func _on_humanoid_pressed() -> void:
-	player.form = "human"
-	sprite.animation = "human_down"
+	set_form("human", loki_music)
 	close()
 
 func _on_horse_pressed() -> void:
-	player.form = "horse"
-	sprite.animation = "horse_down"
+	set_form("horse", horse_music)
 	close()
 
 func _on_bird_pressed() -> void:
-	player.form = "bird"
-	sprite.animation = "bird_down"
+	set_form("bird", loki_music)
 	close()
 
 func _on_jotun_pressed() -> void:
-	player.form = "guard"
-	sprite.animation = "guard_down"
+	set_form("guard", loki_music)
 	close()
+
+func set_form(form: String, music: AudioStreamPlayer) -> void:
+	player.form = form
+	sprite.animation = form + "_down"
+	loki_music.volume_db = -80
+	horse_music.volume_db = -80
+	music.volume_db = 0
 
 func close() -> void:
 	hide()
